0d9e31be2386e3637dad146f13d80e3edd4b3e96
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 14:06:42 -0700

    updates folder model

--- models/folder.js
+++ models/folder.js
@@ -1,47 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const cardSchema = new Schema(
-  {
-    content: {
-      type: String,
-      required: true
-    },
-    translation: {
-      type: String,
-      required: true
-    },
-    difficulty: {
-      type: String,
-      enum: ["New Word", "Tough", "Okay", "Easy", "Fluent"],
-      required: true
-    }
-  },
-  {
-    timestamps: true
-  }
-);
-
-const deckSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-    cards: [cardSchema]
-  },
-  {
-    timestamps: true
-  }
-);
-
 const folderSchema = new Schema(
   {
     name: {
       type: String,
       required: true
     },
-    cards: [deckSchema]
+    decks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Deck"
+      }
+    ],
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
   },
   {
     timestamps: true
