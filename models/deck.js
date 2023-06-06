e526b4bf7d989bf9245dcda09a63fa7dce6bf93f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 14:06:56 -0700

    adds deck model

---
+++ models/deck.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    translation: {
+      type: String,
+      required: true
+    },
+    difficulty: {
+      type: String,
+      enum: ["New Word", "Tough", "Okay", "Easy", "Fluent"],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const deckSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    cards: [cardSchema],
+    folders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Folder"
+      }
+    ],
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
