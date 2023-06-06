b31483b9ab5f8893aa7113dc85dcc092f9bb723f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 14:10:09 -0700

    adds pw hash to user model

--- models/user.js
+++ models/user.js
@@ -3,22 +3,41 @@ const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 6;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      trime: true,
+      minLength: 6,
+      required: true
+    }
   },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    trime: true,
-    minLength: 6,
-    required: true
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      }
+    }
   }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
 });
+
+module.exports = mongoose.model("User", userSchema);
