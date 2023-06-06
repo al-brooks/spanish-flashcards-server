7e49b615494fd7d04dbf3ef9491ae6cbe5c6bd09
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 12:00:12 -0700

    sets up server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+// Configure Settings
+require("dotenv").config();
+require("./config/database");
+
+// Mount Middleware
+
+// AJAX
+app.use(express.json()); // creates req.body
+app.use(morgan("dev"));
+
+// Mount API Routes
+// app.use("/api/users", require("./routes/api/users"));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Express Server is listening on port:${port}`);
+});
