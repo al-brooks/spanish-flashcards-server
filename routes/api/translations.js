c1098ad6c4fffb135f4c7d2c75fca003340ce37f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-07 15:32:01 -0700

    updates comment

--- routes/api/translations.js
+++ routes/api/translations.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const translateCtrl = require("../../controllers/api/translations");
 
-// GET /api/translate/:words
+// GET /api/translate/:search
 router.get("/:search", translateCtrl.getTranslation);
 
 module.exports = router;
