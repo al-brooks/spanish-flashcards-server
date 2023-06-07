cbfdf485b9270c243ca61910c33089664c4ce8a2
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-07 11:44:18 -0700

    adds api translation route

---
+++ routes/api/translations.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const translateCtrl = require("../../controllers/api/translations");
+
+// GET /api/translate/:words
+router.get("/:words", translateCtrl.getTranslation);
+
+module.export = router;
