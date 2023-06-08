4db8e48e5ffbfef65840e9787dbf430a883956f1
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-08 11:52:06 -0700

    adds checktoken + decks route

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ require("./config/database");
 
 // AJAX
 app.use(express.json()); // creates req.body
+app.use(require("./config/checkToken"));
 app.use(cors());
 app.use(morgan("dev"));
 
 // Mount API Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/translate", require("./routes/api/translations"));
+app.use("/api/decks", require("./routes/api/decks"));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
