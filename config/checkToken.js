31f27f2f53dbe253d2cd8f524cf13974c6d4343f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-08 11:50:17 -0700

    fixes token verify bug

--- config/checkToken.js
+++ config/checkToken.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   let token = req.get("Authorization") || req.query.token;
   if (token) {
-    token = token.replace("Bearer", "");
+    token = token.replace("Bearer ", "");
+    console.log(token);
+
     jwt.verify(token, process.env.SECRET, function (err, decoded) {
       req.user = err ? null : decoded.user;
       req.exp = err ? null : new Date(decoded.exp * 1000);
