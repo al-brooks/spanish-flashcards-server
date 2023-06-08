0285a2ac3ca3c3d43f4fe85f21fd50dced12ada1
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-08 15:53:00 -0700

    sets up pulling all decks

--- controllers/api/decks.js
+++ controllers/api/decks.js
@@ -1,8 +1,19 @@
 const Deck = require("../../models/deck");
 
 module.exports = {
+  index,
   create: createDeck
 };
+
+// GET /api/decks
+async function index(req, res) {
+  try {
+    const decks = await Deck.find({ users: req.user });
+    res.json(decks);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
 
 // POST /api/decks
 async function createDeck(req, res) {
--- routes/api/decks.js
+++ routes/api/decks.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const decksCtrl = require("../../controllers/api/decks");
 
+// GET /api/decks
+router.get("/", decksCtrl.index);
+
 // POST /api/decks
 router.post("/", decksCtrl.create);
 
